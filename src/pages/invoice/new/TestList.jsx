b2a6c0a0f0f9701b274bf4b64e3667720cb3566d
Samiul Fahad <samiul_fahad@yahoo.com>
2025-02-07 04:19:56 +0600

    Upadted all dependencies

--- src/pages/invoice/new/TestList.jsx
+++ src/pages/invoice/new/TestList.jsx
@@ -1,16 +1,30 @@
-import { Checkbox } from "@material-tailwind/react"
+import { Checkbox } from "@material-tailwind/react";
 
 const TestList = ({ list, onChange }) => {
   return (
     <>
       <h2 className="w-full text-left text-md">Test List</h2>
       <div className="flex flex-wrap justify-start items-center">
+        {/* {list.map((test) => (
+          <Checkbox
+            key={test._id}
+            label={`${test.name} [${test.price} tk]`}
+            value={test.name}
+            onChange={() => onChange(test)}
+          />
+        ))} */}
+
         {list.map((test) => (
-          <Checkbox key={test._id} label={`${test.name} [${test.price} tk]`} value={test.name} onChange={() => onChange(test)} />
+          <div key={test._id}>
+            <label className="cursor-pointer flex items-center">
+              <input type="checkbox" value={test.name} onChange={() => onChange(test)} className="w-[20px] h-[20px]" />{" "}
+              <span className="px-2">{test.name} [{test.price}tk]</span>
+            </label>
+          </div>
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default TestList
+export default TestList;
